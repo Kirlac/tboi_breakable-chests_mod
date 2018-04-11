e8ce7e80065480256e86d4ae20b62902b9c78534
Rikki Calteaux <rikkicalteaux+@gmail.com>
2018-04-11 20:16:20 +1000

    Moved chest damage logic to its own function

--- tboi_breakable-chests_mod/BreakableChests/main.lua
+++ tboi_breakable-chests_mod/BreakableChests/main.lua
@@ -6,21 +6,38 @@ local CHEST_HIT_POINTS = 12
 local CHEST_KNOCKBACK_MULTIPLIER = 4
 local COLOR_RED = Color(1, 0, 0, 1, 1, 1, 1)
 
-function BreakableChests:onTearUpdate(tear)
+-- Uncomment chest types below to enable them to be shot
+local CHEST_ENTITY_VARIANTS = {
+    -- PickupVariant.PICKUP_CHEST,
+    -- PickupVariant.PICKUP_BOMBCHEST,
+    PickupVariant.PICKUP_SPIKEDCHEST,
+    -- PickupVariant.PICKUP_ETERNALCHEST,
+    PickupVariant.PICKUP_MIMICCHEST,
+    -- PickupVariant.PICKUP_LOCKEDCHEST
+}
+
+local CHEST_DESTROYED_ACTION = ChestDestroyedAction.RANDOM
+
+local ChestDestroyedAction = {
+    NOTHING = 0,
+    SPAWN_POOP = 1,
+    OPEN_CHEST = 2,
+    RANDOM = 3
+}
+
+function BreakableChests:OnTearUpdate(tear)
     local player = Isaac.GetPlayer(0)
     local entities = Isaac:GetRoomEntities()
     for ei, entity in pairs(entities) do
         if entity.Type == EntityType.ENTITY_PICKUP then
-            if entity.Variant == PickupVariant.PICKUP_SPIKEDCHEST then
-                if entity.SubType == ChestSubType.CHEST_CLOSED then
-                    if entity.Position:Distance(tear.Position) < CHEST_HIT_DISTANCE then
-                        entity.HitPoints = entity.HitPoints - player.Damage
-                        entity:AddVelocity(tear.Velocity:__div(CHEST_KNOCKBACK_MULTIPLIER))
-                        tear:Kill()
-                        if entity.HitPoints < 1 then
-                            entity:Kill()
-                        else
-                            entity:SetColor(COLOR_RED, 1, 1, true, true)
+            for _, variant in pairs(CHEST_ENTITY_VARIANTS) do
+                if entity.Variant == variant then
+                    if entity.SubType == ChestSubType.CHEST_CLOSED then
+                        if entity.Position:Distance(tear.Position) < CHEST_HIT_DISTANCE then
+                            local knockback = tear.Velocity:__div(CHEST_KNOCKBACK_MULTIPLIER)
+                            BreakableChests.DamageChest(entity, player.Damage, knockback)
+                            tear:Kill()
+                            return
                         end
                     end
                 end
@@ -29,8 +46,17 @@ function BreakableChests:onTearUpdate(tear)
     end
 end
 
+function BreakableChests.DamageChest(chest, damage, knockback)
+    chest.HitPoints = chest.HitPoints - damage
+    chest:AddVelocity(knockback)
+    if chest.HitPoints < 1 then
+        chest:Kill()
+    else
+        chest:SetColor(COLOR_RED, 1, 1, true, true)
+    end
+end
 
-function BreakableChests:onPickupInit(pickup)
+function BreakableChests:OnPickupInit(pickup)
     if pickup.Variant == PickupVariant.PICKUP_SPIKEDCHEST
     or pickup.Variant == PickupVariant.PICKUP_MIMICCHEST then
         pickup.HitPoints = CHEST_HIT_POINTS
